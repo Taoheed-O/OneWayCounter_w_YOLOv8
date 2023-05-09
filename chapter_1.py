b9cf70b877d060173b89fb6d898cde1a1072c632
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-05-09 13:11:23 +0100

    code review

--- chapter_1.py
+++ chapter_1.py
@@ -29,7 +29,9 @@ while True:
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
             # print(x1, x2, x3, x4)
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            # cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            w, h = x2 - x1, y2 - y1
+            cvzone.cornerRect(img, (x1, y1,w, h))
 
     cv2.imshow('image', img)
 
