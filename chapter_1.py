cfa5cfcac63d3811034951baf932ce9a83cf13c8
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-05-09 10:42:22 +0100

    yolov8

--- chapter_1.py
+++ chapter_1.py
@@ -1,1 +1,8 @@
 # getting started with YOLO
+from ultralytics import YOLO
+import cv2
+
+# initializing model
+model = YOLO('yolov8n.pt')
+results = model('images/students.jpg', show=True)
+cv2.waitKey(0)
