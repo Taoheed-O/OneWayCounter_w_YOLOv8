fcd674fa0f629a3bb52803329b6013550607b135
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-05-09 11:24:44 +0100

    yolov8 l

--- chapter_1.py
+++ chapter_1.py
@@ -3,6 +3,6 @@ from ultralytics import YOLO
 import cv2
 
 # initializing model
-model = YOLO('yolov8n.pt')
+model = YOLO('../Yolo-Weights/yolov8l.pt')
 results = model('images/students.jpg', show=True)
 cv2.waitKey(0)
