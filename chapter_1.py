9bc354764fd77cf49091a6d9d7f5ff106265c235
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-05-10 15:43:57 +0100

    classnames list added manually

--- chapter_1.py
+++ chapter_1.py
@@ -19,6 +19,18 @@ cap.set(4, 720)
 model = YOLO('Yolo-Weights/yolov8l.pt')
 
 
+classnames = ['person', 'bicycle', 'car', 'motorbike', 'aeroplane', 'bus', 'train', 'truck', 'boat',
+              'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird','cat',
+              'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella',
+              'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kites', 'baseball bat',
+              'baseball glove', 'skateboard', 'surf board', 'tennis racket', 'bottle', 'wine glass', 'cup',
+              'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwitch', 'orange', 'broccoli',
+              'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'sofa', 'pottedplant', 'bed',
+              'dinningtable', 'toilet', 'tvmonitor', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+              'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+              'teddy bear', 'hair drier', 'toothbrush']
+
+
 while True:
     source, img = cap.read()
     results = model(img, stream=True)
