b4cc7d8def8a983b1bebf235d8a3541bd07ac586
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-06-05 11:12:58 +0100

    finishing touches

--- Counter/personCounter.py
+++ Counter/personCounter.py
@@ -41,6 +41,8 @@ totalCounts = []
 
 while True:
     source, img = cap.read()
+    imgGraphics = cv2.imread('images/person-walking.png', cv2.IMREAD_UNCHANGED)
+    img = cvzone.overlayPNG(img, imgGraphics, (0, 0))
     # imgRegion = cv2.bitwise_and(img, mask)
     results = model(img, stream=True)
 
@@ -91,7 +93,7 @@ while True:
                 totalCounts.append(id)
                 cv2.line(img, (limits[0], limits[1]),(limits[2], limits[3]), (0, 255, 0), 5)
 
-    cvzone.putTextRect(img, f"counts: {len(totalCounts)}", (50, 50))
+    cv2.putText(img, f"counts: {len(totalCounts)}", (255, 150), cv2.FONT_HERSHEY_DUPLEX, 5, (255, 0, 255), 8)
 
     img = cv2.resize(img, (frameWidth, frameHeight))
     cv2.imshow('image', img)
